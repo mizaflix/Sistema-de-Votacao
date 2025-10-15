0d118b308d3a72bd7cb89efce48d7eef93ed3a5a
Mizael <mizaflix@gmail.com>
2025-10-15 20:08:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -187,5 +187,7 @@ def admin_update_config():
 
 
 if __name__ == '__main__':
+    import os
     inicializar_arquivos()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
